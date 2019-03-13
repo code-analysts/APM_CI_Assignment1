ff90efa0616f0666eb92f92d146fefe3aace4dd0
Farrah Marie Chavez <pangcaberte@gmail.com>
2019-03-13 20:11:08 +1300

    sdDeleted node_modules

---
+++ app.js
@@ -0,0 +1,94 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin'
+});
+
+var app = express();
+
+// For Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+/*app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/login', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.get('/register', function(request, response) {
+	response.sendFile(path.join(__dirname + '/register.html'));
+}); */
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.post('/submit-registration', function(request, response) {
+	var username = request.body.username;
+	var fullname = request.body.fullname;
+	var email = request.body.email;
+	var password = request.body.password;
+	
+	connection.query('INSERT INTO accounts (username, fullname, email, password) VALUES("' + [username] + '","' + [fullname] + '","' + [email] + '","' + [password] + '")');
+	
+	response.redirect('/');
+});
+
+app.listen(3000);
+
